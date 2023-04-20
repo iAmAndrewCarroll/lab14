efcc71f91de6d3d56589b73cb620e160e1c8c501
Andrew Thomas Carroll <athomascarroll@gmail.com>
2023-04-20 13:36:23 -0700

    tiffani did a great job

--- js/app.js
+++ js/app.js
@@ -20,14 +20,18 @@ AppState.prototype.instantiateProducts = function () {
 
 AppState.prototype.saveToLocalStorage = function () {
   // TODO: Fill in this instance method to save product data to local storage
+  localStorage.setItem('oddProducts', JSON.stringify(this.allProducts));
 }
 
 AppState.prototype.loadItems = function () {
 
   // TODO: Update this instance method to retrieve data from local storage instead of creating new Products on each page load
-
-  this.instantiateProducts();
-
+  let storageProducts = localStorage.getItem('oddProducts');
+  if (storageProducts) {
+    this.allProducts = JSON.parse(storageProducts);
+  } else {
+    this.instantiateProducts();
+  }
 }
 
 
